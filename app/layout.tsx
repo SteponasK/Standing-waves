056903c81224d18fc453ae6c4e155c1639989353
Steponas Kaminskas <132608893+SteponasK@users.noreply.github.com>
2025-03-26 12:31:00 +0200

    Update app title to 'Standing wave'

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Stovinčioji banga',
-  description: 'Stovinčioji banga',
-  generator: 'Stovinčioji banga',
+  title: 'Standing wave',
+  description: 'Standing wave',
+  generator: 'Standing wave',
 }
 
 export default function RootLayout({
